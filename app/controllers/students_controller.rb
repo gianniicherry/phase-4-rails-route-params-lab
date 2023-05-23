c9b7148f6087eec20059f36fc569004e39f400e7
Gianni <gianniicherry@gmail.com>
2023-05-22 17:28:57 -0700

    commit tree 75e9e7f5be533c88c895b7d411adbaa81f2d9471

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def first
+    first_student = Student.find(params[:id])
+    render json: first_student
+  end
+
 end
